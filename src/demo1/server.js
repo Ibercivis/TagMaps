a405190235269ef472732a251e68384fc9b1f447
Juanjo Molinero <jjmolinero@gmail.com>
2012-05-24 18:15:35 +0200

    Added Zaragoza and universidad keywords

--- src/demo1/server.js
+++ src/demo1/server.js
@@ -1,5 +1,8 @@
 var boxes = '-18.16,27.63,-13.4,30.0,-7.523,36.0,-1.6274,38.7289,-7.5416,37.35,4.33,40.48,-7.08,40.48,4.33,41.807,-9.3,41.807,-0.73,43.8,-0.73,41.807,3.34,43.0,-2.9755,35.2560,-2.91,35.337,-5.3822,35.8715,-5.2783,35.9180';
 
+var keywords = 'unizar,@unizar,#unizar,zaragoza,iberus,manolo lopez,universidad de zaragoza,prueba de node';
+var users = '';
+
 var util = require('util');
 var twitter = require('immortal-ntwitter');
 
@@ -10,8 +13,15 @@ var tweet = twitter.create({
     access_token_secret: 'CmHOuzo5wuBXrh3NIs1BQw0l8l9R6sOdiFMf1vBJU'
 });
 
-tweet.stream('statuses/filter', {'locations': boxes}, function(stream) {
+//tweet.stream('statuses/filter', {'locations': boxes}, function(stream) {
+//     stream.on('data', function(data) {
+// 	console.log('@' + data.user.screen_name + ' : ' + data.text);
+//     });
+// });
+
+tweet.stream('statuses/filter', {'track': keywords}, function(stream) {
     stream.on('data', function(data) {
-	console.log('@' + data.user.screen_name + ' : ' + data.text);
+ 	//console.log('@' + data.user.screen_name + ' : ' + data.text);
+	console.log(data);
     });
 });
