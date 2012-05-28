c9f518a280b182c5069c7a0cb8763e21f12d15a4
BIFI <info@bifi.es>
2012-05-28 17:31:15 +0200

    hacia manejo completo de la BBDD. Seguimos o factorizamos en objetos?

--- src/demo1/server.js
+++ src/demo1/server.js
@@ -3,6 +3,7 @@ var boxes = '-18.16,27.63,-13.4,30.0,-7.523,36.0,-1.6274,38.7289,-7.5416,37.35,4
 var keywords = 'unizar,@unizar,#unizar,zaragoza,bifi,ibercivis,iberus,chema gimeno,manolo lopez,universidad de zaragoza';
 var users = '';
 
+var asincrono=require('async');
 var util = require('util');
 var twitter = require('immortal-ntwitter');
 var fs = require('fs');
@@ -24,7 +25,7 @@ var serverConfig = {
 var server = new Server(serverConfig);
 var db = new Db("../databases/twzaragoza",server,dbConfig);
 //al que se le ocurrio que Uppercase/Lowercase era una buena manera de
-//distinguir variables, tendrian que colgarlo -- AR
+//distinguir variables y creadores, tendrian que colgarlo -- AR
 
 
 var tweet = twitter.create({
@@ -39,22 +40,127 @@ var tweet = twitter.create({
 // 	console.log('@' + data.user.screen_name + ' : ' + data.text);
 //     });
 // });
-var my_file = fs.openSync('stream_unizar.log', 'a');
+//var my_file = fs.openSync('stream_unizar.log', 'a');
+
+
+
+function comprueba(tableclass, id, callback) {
+   db.command("SELECT FROM "+tableclass+" where id = "+id, function(err,results) {
+      if (results.length) == 0 {
+         callback(); //null result
+      }
+      else {
+         callback(results[0]["@rid"]);  //o es 1??
+      } 
+   }
+}
+
+
+
+function savePlace(place,callback) {
+  //comprueba que no existe el id; si existe devuelve el rid
+  comprueba("lugar",place.id,hold.method)
+  callback(null,rid) 
+}
+
+function saveAuthor(authordata,callback) {
+  //comprueba que no existe el id; si existe devuelve el rid
+
+  comprueba("usuario",author.id,hold.method)
+  callback(null,rid);//no error, devolvemos el rid
+}
+
+
+function createEdges(tweetRid, entities) {
+  //TO DO NEXT
+  //de momento solo hashtags. Podemos hacer un
+  //for key en entities, y considerarlos todos
+  //de la misma clase, o hacer un caso
+  //especial con mentions, que pueden ser authors o no
+}
+
+
+function saveTweet(data,callback) {
+
+  if !(this.ok) { //nos llaman sin ser objeto, nos objetivamos
+                  //en la verificacion
+      var hold={}
+      hold.data=data
+      hold.method=saveTweet
+      hold.callback=callback
+      hold.ok=true
+      comprueba("mensaje",data.id,hold.method)
+      return 
+  }
+ 
+  //nos llaman como objeto 
+  callback=this.callback;
+  if (data){ //ya hay un rid, en data!
+       callback(null,data)
+       return
+    }
+     
+  //no hay rid, es todo nuevo
+  data=this.data
+  
+  var sustituciones={};
+  //sustituye autor 
+     sustituciones.user= function(callback){
+       saveAuthor(data.user, callback);
+     });
+  //sustituye place si lo hay
+    if (data.place) {
+      sustituciones.place= function(callback){
+       savePlace(data.place, callback);
+     });
+    }
+  //sustituye retuit si lo hay (esto es recursivo)
+    if (data.place) {
+      sustituciones.retweeted_status=function(callback){
+       saveTweet(data.retweeted_status, callback);
+     });
+    }
+  
+  asincrono.parallel(sustituciones, function(err,results) {
+     if (err) {
+       console.log("fallo el paralelo "+err)
+     } else {
+        //acepta rids, graba dato ... 
+        for (key in results) {
+          data[key]=results[key]; 
+        }
+        data["@class"]="mensaje";
+          //TO DO: la fecha, vale tal como esta?
+        db.save(data, function(err, data) {
+           if (err) {
+              console.log("db fail "+ err);
+           } else {
+              console.log("result:"+data["@rid"]);
+              // ... y crea entidades; lo hacemos aqui por si hay llamadas recursivas a saveTweet 
+              createEdges(data["@rid"], data["entities"]);
+              // TO DO: podriamos plantearnos Edges especiales:
+              //     if (in_reply_to_status_id) ...
+              //     if (in_reply_to_user_id) ....
+              //     if (retweeted_status) ....
+              callback(null,data["@rid"]);
+              return;
+           }
+        });
+      }
+  });
+  // si llegamos aqui, mal vamos
+  callback("some error has happened");
+}
+
+
 
 db.open(function(err,result){
   tweet.stream('statuses/filter', {'track': keywords}, function(stream) {
     stream.on('data', function(data) {
-	fs.writeSync(my_file, JSON.stringify(data), null);
-	fs.writeSync(my_file, '\n', null);
-	fs.fsyncSync(my_file);
-        data["@class"]="patata";
-        db.save(data, function(err, data) {
-          if (err) { 
-             console.log("db fail "+ err);
-          } else {
-          console.log("result:"+data["@rid"])
-          }
-        });
+//	fs.writeSync(my_file, JSON.stringify(data), null);
+//	fs.writeSync(my_file, '\n', null);
+//	fs.fsyncSync(my_file);
+        saveTweet(data,console.log);  
     });
   });
 });
