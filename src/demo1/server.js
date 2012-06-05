95591052fecebf8373dba295b5fd1776e487bb28
Alejandro Rivero <al.rivero@gmail.com>
2012-06-06 01:12:50 +0300

    Hay que llevar el dato completo porque createEdge sustituye en vez de solo hacer un update de los campos relevantes.

--- src/demo1/server.js
+++ src/demo1/server.js
@@ -126,40 +126,40 @@ function saveAuthor(authordata, finalcall) {
 }
 
 
-function saveLink(fromRid, text) {
+function saveLink(fromData, text) {
   //Como no hay un insert ignore hay que asegurarse bien. Por variar
   //de tema, vamos a hacerlo con waterfall.
-  console.log ("try to save from "+fromRid+ " to "+ text);
+  console.log ("try to save from "+fromData.@rid+ " to "+ text);
   asincrono.waterfall([
     function(cb){
         db.command("SELECT FROM hashtag where name = '" + text +"'", function(err,results) {
             if (results.length == 0) {
               cb(null,null);
             }      else {
-              cb(null,results[0]["@rid"]); 
+              cb(null,results[0]); 
             }
            }
         );
       },
-    function(rid,cb){ 
-       if (rid) {
-         cb(null,rid); 
+    function(toData,cb){ 
+       if (toData) {
+         cb(null,toData); 
        } else {
          data = {"@class": "hashtag",name: text}
          db.save(data, function (err,data) {  if (err) {
           console.log ("db vertex fail"+JSON.stringify(err)+JSON.stringify(data));
            cb(err);
           } else {
-           cb(null,data["@rid"]);
+           cb(null,data);
           }
          });
         }
        },
-    function(rid,cb) {
-          console.log ("ok to save from "+fromRid+ " to "+ rid);
+    function(toData,cb) {
+          console.log ("ok to save from "+fromData.@rid+ " to "+ toData.@rid);
           db.createEdge(
-            {"@rid":fromRid, "@class":"mensaje"},
-            {"@rid":rid, "@class": "hashtag"},
+            fromData,  // {"@rid":fromRid, "@class":"mensaje"},
+            toData, // {"@rid":rid, "@class": "hashtag"},
             function(err,result) { if (err) { 
                                      console.log("Edge error:"+JSON.stringify(err));
                                      cb(err);
@@ -173,7 +173,7 @@ function saveLink(fromRid, text) {
 }
 
 
-function createEdges(tweetRid, entities) {
+function createEdges(tweetData, entities) {
   //de momento solo hashtags. Podemos hacer un
   //for key en entities, y considerarlos todos
   //de la misma clase, o hacer un caso
@@ -181,7 +181,7 @@ function createEdges(tweetRid, entities) {
   for (var i in entities.hashtags) { 
      with (entities.hashtags[i]) {
        console.log("ht:"+text+" en "+indices);
-       saveLink(tweetRid,text);
+       saveLink(tweetData,text);
        }
   }
 }
@@ -243,7 +243,7 @@ function saveTweet(data, finalcall) {
 		    console.log("result:"+data["@rid"]);
                     console.log("author is "+data.user);
 		    // ... y crea entidades; lo hacemos aqui por si hay llamadas recursivas a saveTweet 
-		    createEdges(data["@rid"], data["entities"]);
+		    createEdges(data, data["entities"]);
 		    // TO DO: podriamos plantearnos Edges especiales:
 		    //     if (in_reply_to_status_id) ...
 		    //     if (in_reply_to_user_id) ....
